7cd47c0bbd5e22164bd97f4373cebb22271af21d
RadeshL <radeshl2006@gmail.com>
2025-07-30 11:07:39 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
 import Teams from './pages/Teams';
+import Events from './pages/EventsPage.jsx'
 
 function App() {
   return (
     <>
+      <Events />
       <Header />
       <Teams />
     </>
