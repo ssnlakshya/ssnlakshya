2f3e30bc6c88125913f7dc22d5f2f5fd51e9ef48
RadeshL <radeshl2006@gmail.com>
2025-08-02 19:35:41 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './Events_page_index.css';
+import EventsPage from './pages/EventsPage.jsx';
+import Header from './components/Header.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<EventsPage />} />
+        <Route path="/events" element={<EventsPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
