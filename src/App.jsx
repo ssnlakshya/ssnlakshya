75877a179db5a2411d990cc3b1696ce326ed465f
RadeshL <radeshl2006@gmail.com>
2025-08-02 19:34:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import Header from './components/Header';
-import Teams from './pages/Teams';
-import Events from './pages/EventsPage.jsx'
+import Header from './components/Header.jsx';
+import Teams from './pages/Teams.jsx';
+import Events from './pages/EventsPage.jsx';
+import MoneyAnimation from './components/MoneyAnimation.jsx';
+import SlickSlider from './components/SlickSlider.jsx';
 
 function App() {
   return (
     <>
+      <Header />  
+      <MoneyAnimation />
+      <SlickSlider />
       <Events />
-      <Header />
       <Teams />
     </>
   );
 }
 
 export default App;
+
